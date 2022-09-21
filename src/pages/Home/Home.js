95fdf8654c45a20478535b20709a101b08e708cb
Marcin Garski <mgarski@tutanota.com>
2022-09-21 18:29:38 -0400

    Initialization project

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Home.module.css';
+
+function Home(props) {
+  return (
+    <div>Home</div>
+  )
+}
+
+
+
+export default Home
---
+++ src/pages/Hotel/Hotel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Hotel(props) {
+  return (
+    <div>Hotel</div>
+  )
+}
+
+
+
+export default Hotel
