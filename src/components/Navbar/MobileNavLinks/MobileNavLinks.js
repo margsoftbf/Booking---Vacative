83b38c2fc36c0806d0b83dccbe52d48472b97448
Marcin Garski <mgarski@tutanota.com>
2022-09-24 11:18:12 -0400

    some changes on mobile version

--- src/components/Navbar/MobileNavLinks/MobileNavLinks.js
+++ src/components/Navbar/MobileNavLinks/MobileNavLinks.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react'
 import style from './MobileNavLinks.module.css'
-import { MenuToggle } from "../MenuToggle/MenuToggle";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import Hamburger from 'hamburger-react'
 
 export function MobileNavLinks(props) {
-    const [isOpen, setOpen] = useState(false);
-    return (
-        <div className={`${style.navLinksContainer}`}>
 
+
+    const [showMenu, setShowMenu] = useState(false)
+    const [isOpen, setOpen] = useState(false)
+    let menu
+
+    if (isOpen) {
+
+        menu =
+            <div className={`${style.navLinksContainer}`}>
                 <ul className={`${style.navLinksWrapper}`}>
                     <li className={`${style.navLinkItem}`}>
                         <a className={`${style.navLink}`} href='#'>Stays</a>
@@ -25,9 +32,41 @@ export function MobileNavLinks(props) {
                         <a className={`${style.navLink}`} href='#'>Airport taxis</a>
                     </li>
                 </ul>
-           
-        </div>
+            </div>
+    }
+    return (
+        <nav>
+            <Hamburger toggled={isOpen} toggle={setOpen} />
+            <span className="text-xl">
+                <FontAwesomeIcon
+                    icon={faBars}
+                    onClick={() => setShowMenu(!showMenu)}
+                />
+            </span>
+
+            {menu}
+        </nav>
+
     )
 }
 
 
+{/* <div className={`${style.navLinksContainer}`}>
+<ul className={`${style.navLinksWrapper}`}>
+    <li className={`${style.navLinkItem}`}>
+        <a className={`${style.navLink}`} href='#'>Stays</a>
+    </li>
+    <li className={`${style.navLinkItem}`}>
+        <a className={`${style.navLink}`} href='#'>Flights</a>
+    </li>
+    <li className={`${style.navLinkItem}`}>
+        <a className={`${style.navLink}`} href='#'>Car rentals</a>
+    </li>
+    <li className={`${style.navLinkItem}`}>
+        <a className={`${style.navLink}`} href='#'>Attractions</a>
+    </li>
+    <li className={`${style.navLinkItem}`}>
+        <a className={`${style.navLink}`} href='#'>Airport taxis</a>
+    </li>
+</ul>
+</div> */}
