a6a2f2bb2e231cd41d0dd761b67c00cbc6fe7cfa
Marcin Garski <mgarski@tutanota.com>
2022-09-25 17:37:08 -0400

    Finish styling searchbar, plus data range picker

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 import style from './Header.module.css'
+import { format } from "date-fns"
+import { DateRange } from 'react-date-range';
+
 import {
     faUserGroup,
     faCalendarDays,
@@ -8,6 +13,30 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Header(props) {
+    const [openDate, setOpenDate] = useState(false)
+    const [date, setDate] = useState([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: 'selection'
+        }
+    ]);
+
+    const [openOptions, setOpenOptions] = useState(false)
+    const [options, setOptions] = useState({
+        adult: 1,
+        children: 0,
+    })
+
+    const handleOption = (name, operation) => {
+        setOptions((prev) => {
+            return {
+                ...prev,
+                [name]: operation === "i" ? options[name] + 1 : options[name] - 1,
+            };
+        });
+    };
+
     return (
         <div className={`${style.header}`}>
             <div className={`${style.headerContainer}`}>
@@ -23,10 +52,39 @@ function Header(props) {
                         <FontAwesomeIcon className={`${style.searchIcon}`} icon={faMagnifyingGlass} /> <input type='text' placeholder='Where are you going?' className={`${style.headerSearchbar}`}></input>
                     </div>
                     <div className={`${style.headerSearchItem}`}>
-                        <FontAwesomeIcon className={`${style.searchIcon}`} icon={faCalendarDays} /> <span className={`${style.headerSearchText}`}>Date to date</span></div>
+                        <FontAwesomeIcon className={`${style.searchIcon}`} icon={faCalendarDays} /> <span onClick={() => setOpenDate(!openDate)} className={`${style.headerSearchText}`}>{`${format(date[0].startDate, "MM/dd")} to ${format(
+                            date[0].endDate,
+                            "MM/dd"
+                        )}`}</span>
+                        {openDate && <DateRange
+                            editableDateInputs={true}
+                            onChange={item => setDate([item.selection])}
+                            moveRangeOnFirstSelection={false}
+                            ranges={date}
+                            className={`${style.dateRange}`}
+                        />}
+                    </div>
                     <div className={`${style.headerSearchItem}`}>
                         <FontAwesomeIcon className={`${style.searchIcon}`} icon={faUserGroup} />
-                        <span className={`${style.headerSearchText}`}>2 adults 2 children 1 room</span></div>
+                        <span onClick={() => setOpenOptions(!openOptions)} className={`${style.headerSearchText}`}>{`${options.adult} adult · ${options.children} children`}</span>
+                        {openOptions && <div className={`${style.options}`}>
+                            <div className={`${style.optionsItem}`}>
+                                <div className={`${style.leftSide}`}><span className={`${style.optionText}`}>Adult</span></div>
+                                <div className={`${style.rightSide}`}><button className={`${style.optionCounterButton}`} onClick={() => handleOption("adult", "d")} disabled={options.adult <= 1}>-</button>
+                                    <span className={`${style.optionCountNumber}`}>{options.adult}</span>
+                                    <button className={`${style.optionCounterButton}`} onClick={() => handleOption("adult", "i")}>+</button></div>
+
+                            </div>
+                            <div className={`${style.optionsItem}`}>
+                                <div className={`${style.leftSide}`}><span className={`${style.optionText}`}>Children</span></div>
+                                <div className={`${style.rightSide}`}>
+                                    <button className={`${style.optionCounterButton}`} onClick={() => handleOption("children", "d")} disabled={options.children <= 0}>-</button>
+                                    <span className={`${style.optionCountNumber}`}>{options.children}</span>
+                                    <button className={`${style.optionCounterButton}`} onClick={() => handleOption("children", "i")}>+</button>
+                                </div>
+                            </div>
+                        </div>}
+                    </div>
                     <div className={`${style.headerSearchItem}`}>
                         <button className={`${style.headerSearchBtn}`}>Search</button></div>
                 </div>
--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar';
 
 
 function List(props) {
   return (
-    <div>List</div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
